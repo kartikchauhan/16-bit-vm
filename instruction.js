d695192c5c0661f208221232b533c10027863b75
kartikchauhan <chauhan.kartik25@gmail.com>
2020-07-11 15:19:32 +0000

    add PSH_LIT, PSH_REG, POP instructions

--- instruction.js
+++ instruction.js
@@ -4,6 +4,9 @@ const MOV_REG_MEM = 0x12;
 const MOV_MEM_REG = 0x13;
 const ADD_REG_REG = 0x14;
 const JMP_NOT_EQ  = 0x15;
+const PSH_LIT     = 0x16;
+const PSH_REG     = 0x17;
+const POP         = 0x18;
 
 module.exports = {
     MOV_LIT_REG,
@@ -11,5 +14,8 @@ module.exports = {
     MOV_REG_MEM,
     MOV_MEM_REG,
     ADD_REG_REG,
-    JMP_NOT_EQ
+    JMP_NOT_EQ,
+    PSH_LIT,
+    PSH_REG,
+    POP
 };

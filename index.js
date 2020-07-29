9bd7674fe9ae497a058aa626d548684e2b94641b
kartikchauhan <chauhan.kartik25@gmail.com>
2020-07-29 02:54:42 +0000

    add instructions for stack operations

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const {
     MOV_REG_MEM,
     MOV_MEM_REG,
     ADD_REG_REG,
-    JMP_NOT_EQ
+    JMP_NOT_EQ,
+    PSH_LIT,
+    PSH_REG,
+    POP
 } = require('./instruction');
 
 const IP  = 0;
@@ -20,34 +23,32 @@ const writableBytes = new Uint8Array(memory.buffer);
 const cpu = new CPU(memory);
 let i = 0;
 
-writableBytes[i++] = MOV_MEM_REG;
-writableBytes[i++] = 0x01;
-writableBytes[i++] = 0x00; // 0x0100
+writableBytes[i++] = MOV_LIT_REG;
+writableBytes[i++] = 0x51;
+writableBytes[i++] = 0x51;
 writableBytes[i++] = R1;
 
 writableBytes[i++] = MOV_LIT_REG;
-writableBytes[i++] = 0x00;
-writableBytes[i++] = 0x01; // 0x0001
+writableBytes[i++] = 0x42;
+writableBytes[i++] = 0x42;
 writableBytes[i++] = R2;
 
-writableBytes[i++] = ADD_REG_REG;
+writableBytes[i++] = PSH_REG;
 writableBytes[i++] = R1;
+
+writableBytes[i++] = PSH_REG;
 writableBytes[i++] = R2;
 
-writableBytes[i++] = MOV_REG_MEM;
-writableBytes[i++] = ACC;
-writableBytes[i++] = 0x01;
-writableBytes[i++] = 0x00; // 0x0100
+writableBytes[i++] = POP;
+writableBytes[i++] = R1;
 
-writableBytes[i++] = JMP_NOT_EQ;
-writableBytes[i++] = 0x00;
-writableBytes[i++] = 0x03; // 0x0003
-writableBytes[i++] = 0x00;
-writableBytes[i++] = 0x00; // 0x0000
+writableBytes[i++] = POP;
+writableBytes[i++] = R2;
 
 cpu.debug();
 cpu.viewMemoryAt(cpu.getRegister('ip'));
-cpu.viewMemoryAt(0x0100);
+// cpu.viewMemoryAt(0x0100);
+cpu.viewMemoryAt(0xffff - 1 - 6); // Since the stack grows upwards, and viewMemoryAt shows following 7 bytes, we subtract 6 from the address.
 console.log("\n\n");
 
 const rl = readline.createInterface({
@@ -59,6 +60,7 @@ rl.on('line', () => {
     cpu.step();
     cpu.debug();
     cpu.viewMemoryAt(cpu.getRegister('ip'));
-    cpu.viewMemoryAt(0x0100);
+    // cpu.viewMemoryAt(0x0100);
+    cpu.viewMemoryAt(0xffff - 1 - 6);
     console.log("\n\n");
 });

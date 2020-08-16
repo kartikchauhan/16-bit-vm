0ce05d8897a069dba02f80ab125c6a0c2be6a23d
kartikchauhan <chauhan.kartik25@gmail.com>
2020-08-16 16:04:16 +0000

    add commands eraseScreen, setBold, setRegular

--- screen-device.js
+++ screen-device.js
@@ -1,6 +1,19 @@
+const eraseScreen = () => {
+    process.stdout.write('\x1b[2J');
+}
+
 const moveTo = (x, y) => {
     process.stdout.write(`\x1b[${y};${x}H`);
 }
+
+const setBold = () => {
+    process.stdout.write('\x1b[1m');
+}
+
+const setRegular = () => {
+    process.stdout.write('\x1b[0m');
+}
+
 
 const createScreenDevice = () => {
     return {
@@ -9,6 +22,15 @@ const createScreenDevice = () => {
         setUint16: (address, data) => {
             const characterValue = data & 0x00ff;
             const character = String.fromCharCode(characterValue);
+            const command = (data & 0xff00) >> 8;
+
+            if(command === 0xff) {
+                eraseScreen();
+            } else if(command === 0x01) {
+                setBold();
+            } else if(command === 0x02) {
+                setRegular();
+            }
 
             const x = (address % 16) + 1;
             const y = Math.floor(address / 16) + 1;

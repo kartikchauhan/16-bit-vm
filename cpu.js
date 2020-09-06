e079bbc9000d8dbc7e1320a8f38ea0e1914663ac
kartikchauhan <chauhan.kartik25@gmail.com>
2020-09-06 19:18:35 +0530

    add case statements SUB_REG_REG, MUL_LIT_REG

--- cpu.js
+++ cpu.js
@@ -255,6 +255,22 @@ class CPU {
                 return;
             }
 
+            case SUB_REG_REG: {
+                const r1 = (this.fetch() % this.registers.length) * 2;
+                const r2 = (this.fetch() % this.registers.length) * 2;
+                const r1Value = this.registerMem.getUint16(r1);
+                const r2Value = this.registerMem.getUint16(r2);
+                this.setRegister('acc', r1Value - r2Value);
+                return;
+            }
+
+            case MUL_LIT_REG: {
+                const literal = this.fetch16();
+                const register = (this.fetch() % this.registers.length) * 2;
+                const value = this.registerMem.getUint16(register);
+                this.setRegister('acc', literal * value);
+            }
+
             case JMP_NOT_EQ: {
                 const literal = this.fetch16();
                 const address = this.fetch16();
